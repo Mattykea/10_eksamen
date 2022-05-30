52a4c58c4a4bcdb98a85bd7de68d955e65d87d8e
Mattykea <math764n@stud.kea.dk>
2022-05-30 09:50:08 +0200

    test

--- wp-content/themes/inspiro-child/page-bonne-detalje.php
+++ wp-content/themes/inspiro-child/page-bonne-detalje.php
@@ -12,43 +12,36 @@
  * @package Inspiro
  * @subpackage Inspiro_Lite
  * @since Inspiro 1.0.0
- * @version 1.0.1
+ * @version 1.0.0
  */
 
 get_header(); 
 ?>
 
-   <template>
-      <article class="styleanbefalinger">
-        <img src="" alt="" />
-        <h2></h2>
-        <p class="tekst"></p>
-      </article>
-    </template>
-
 	<div id="primary" class="content-area">
-                
- 
+
+        <main id="main" class="site-main">	
+
+            <template>
+                <article class="styledetalje">
+                    <img src="" alt="" />
+                    <h2></h2>
+                    <p class="tekst"></p>
+                </article>
+            </template>
+
     
-		<main id="main" class="site-main">	
-      <nav id="filtrering">
-      </nav>
-      <h1></h1>
             <section class="anbefalingcontainer"></section>
-    </main><!-- #main -->
+        </main><!-- #main -->
 
 <script>
-    "use strict";
        console.log("nu starter script");
       const container = document.querySelector(".anbefalingcontainer");
       const temp = document.querySelector("template");
 
-      const url = "https://mathildese.dk/kea/10_eksamen/wp-json/wp/v2/anbefaling?per_page=100";
-      const catUrl = "https://mathildese.dk/kea/10_eksamen/wp-json/wp/v2/categories";
+      const url = "https://mathildese.dk/kea/10_eksamen/wp-json/wp/v2/anbefaling/"+<?php echo get_the_ID() ?>;
 
       let data = [];
-      let categories; 
-      let filterAnbefaling = "alle";
 
       let filter = "alle";
       const filterknapper = document.querySelectorAll("button");
@@ -66,7 +59,7 @@ get_header();
       }
 
       function setFilter() {
-        filter = this.dataset.anbefaling;
+        filter = this.dataset.kategori;
         document.querySelector("h1").textContent = this.textContent;
         vis();
       }
@@ -74,44 +67,16 @@ get_header();
       async function hentData() {
         console.log("hentData");
         const respons = await fetch(url);
-        const catData = await fetch(catUrl);
         anbefalinger = await respons.json();
-        categories = await catData.json();
-        console.log(categories);
         vis();
-        opretKnapper();
       }
-
-      function opretKnapper() {
-
-        categories.forEach(cat =>{
-          document.querySelector("#filtrering").innerHTML += `<button class="filter" data-anbefaling="${cat.id}">${cat.name}</button>`
-        })
-
-        addEventListenersToButtons();
-      }
-
-      function addEventListenersToButtons(){
-          document.querySelectorAll("#filtrering button").forEach(elm => {
-            elm.addEventListener("click", filtrering);
-          })
-
-      };
-
-      function filtrering() {
-           filterAnbefaling = this.dataset.anbefaling;
-           console.log(filterAnbefaling);
-
-           vis();
-      }
-
       
       function vis() {
         console.log("vis");
         console.log(anbefalinger);
         container.textContent = "";
 
-        anbefalinger.forEach((anbefaling) => {
+        anbefalinger.forEach((kategori) => {
           const klon = temp.cloneNode(true).content;
 
           //FILTRERING
@@ -119,19 +84,15 @@ get_header();
             klon.querySelector(".tekst").textContent = anbefaling.tekst;
             klon.querySelector("h2").textContent = anbefaling.title.rendered;
             klon.querySelector("img").src = anbefaling.billede.guid;
-            klon.querySelector("article").addEventListener("click", () => {location.href = anbefaling.link; })
             container.appendChild(klon);
                }
-          })
+          });
       }
 
-     function visDetaljer (hvad) {
-        location.href = `page-bonne-detalje.php?id=${hvad._id}`;}
-       
+     hentData();
     </script>
 
-    	</div><!-- #primary -->
-
+	</div><!-- #primary -->
 
 <?php
 get_footer();
